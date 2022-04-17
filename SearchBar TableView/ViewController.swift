dc92eb2dd012f7cee4b246f590988caed2386608
Omer Faruk Kilicaslan <omerrr_2001@hotmail.com>
2022-04-17 15:26:08 +0300

    .

--- SearchBar TableView/ViewController.swift
+++ SearchBar TableView/ViewController.swift
@@ -9,11 +9,94 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var tableView: UITableView!
+    @IBOutlet weak var searchBar: UISearchBar!
+    
+    var ulkeler = [String]()
+    
+    var aramaSonucuUlkeler = [String]()
+    
+    var aramaYapiliyorMu = false
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+
+        ulkeler = ["Türkiye", "Almanya", "Japonya"," Rusya", "İtalya", "Güney Kore", "Fransa", "Mısır"]
+        
+        searchBar.delegate = self
+        tableView.delegate = self
+        tableView.dataSource = self
+
     }
 
 
 }
 
+
+extension ViewController:UITableViewDelegate, UITableViewDataSource{
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        
+        if aramaYapiliyorMu {
+            return aramaSonucuUlkeler.count
+        }
+        else{
+            return ulkeler.count
+        }
+        
+        
+        
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "ulkelerHucre", for: indexPath)
+        
+        
+        
+        if aramaYapiliyorMu {
+            cell.textLabel?.text = aramaSonucuUlkeler[indexPath.row]
+        }
+        else{
+            cell.textLabel?.text = ulkeler[indexPath.row]
+        }
+        
+        
+        return cell
+        
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        
+        if aramaYapiliyorMu {
+            print("Seçilen Ülke \(aramaSonucuUlkeler[indexPath.row])")
+        }
+        else{
+            print("Seçilen Ülke \(ulkeler[indexPath.row])")
+        }
+        
+        
+    }
+}
+
+extension ViewController:UISearchBarDelegate{
+    
+    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+        print("Arama Sonucu : \(searchText)")
+        
+        if searchText == "" {
+            aramaYapiliyorMu = false
+        }
+        else{
+            aramaYapiliyorMu = true
+            aramaSonucuUlkeler = ulkeler.filter({$0.lowercased().contains(searchText.lowercased())})
+        }
+        
+        
+        
+        tableView.reloadData()
+    }
+    
+}
